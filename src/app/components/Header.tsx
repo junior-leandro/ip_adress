9678c077e05372695ebadafb7e860974fbd71f59
Junior Oliveira Leandro <contato_jr@outlook.com>
2023-10-19 09:50:39 -0300

    update layout on 1536px wide screens and IP lookup input pattern

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,18 @@
+import Image from "next/image";
 import { Infos } from "./Infos";
+
+import Arrow from  '../../assets/icon-arrow.svg'
 
 export function Header() {
     return (
-        <header className="w-full h-1/3 bg-img_bg_desktop bg-center bg-cover flex flex-col items-center z-10">
+        <header className="w-full h-1/3 bg-img_bg_desktop bg-center bg-cover flex flex-col items-center z-10 2xl:pt-6">
             <h1 className="text-white text-2xl font-bold my-5">IP Address Tracker</h1>
 
             <form className="bg-white w-5/6 h-[50px] max-w-xl flex justify-between rounded-2xl">
-                <input type="text" className="rounded-s-2xl pl-4 py-4 w-full" placeholder="Search for any IP address or domain"></input>
-                <button type="submit" className="bg-black text-white rounded-e-2xl p-4 font-bold"> &gt; </button>
+                <input type="text" className="rounded-s-2xl pl-4 py-4 w-full" placeholder="Search for any IP address or domain" autoComplete="off" pattern="^((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.){3}(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])$"></input>
+                <button type="submit" className="bg-black text-white rounded-e-2xl p-4 font-bold">
+                    <Image src={Arrow} alt="Arrow Search" />
+                </button>
             </form>
 
             <Infos />
--- src/app/components/Infos.tsx
+++ src/app/components/Infos.tsx
@@ -7,7 +7,7 @@ export function Infos() {
     return (
 
         <article className="w-full h-72 mt-7 flex items-center justify-center">
-            <div className="flex flex-col lg:mt-16 lg:flex-row lg:justify-around gap-3 py-4 lg:py-6 w-5/6 max-w-[950px] bg-white rounded-xl shadow-2xl">
+            <div className="flex flex-col  lg:mt-2 2xl:mt-16 lg:flex-row lg:justify-around gap-3 py-4 lg:py-6 w-5/6 max-w-[950px] bg-white rounded-xl shadow-2xl">
                 <div className="flex flex-col items-center lg:items-start lg:justify-start lg:gap-2 lg:pl-10 lg:w-1/4">
                     <span className="text-gray-400 text-xs">IP ADRESS</span>
                     <span className="text-black text-xl font-semibold">{IpAdress}</span>
